08418c377104d24d64590e5a1e8219bc95d04042
ruanpablom <pm.ruan@gmail.com>
2015-06-10 12:33:25 -0300

    Enviando arquivos

---
+++ Makefile
@@ -0,0 +1,22 @@
+CC=gcc
+CFLAGS=-c -Wall
+MATH=-lm
+OBJECTS=algorithm.o mersenne.o sosfunctions.o
+
+all: alg
+
+alg: $(OBJECTS)
+	$(CC) $(OBJECTS) -o alg $(MATH)
+
+algorithm.o: algorithm.c
+	$(CC) $(CFLAGS) algorithm.c
+
+mersenne.o: mersenne.c
+	$(CC) $(CFLAGS) mersenne.c $(MATH)
+
+sosfunctions.o: sosfunctions.c
+	$(CC) $(CFLAGS) sosfunctions.c $(MATH)
+
+clean: 
+	rm $(OBJECTS) alg
+
---
+++ sos.h
@@ -0,0 +1,41 @@
+#ifndef SOSFUNCTIONS_H
+#define SOSFUNCTIONS_H
+
+/* Control Parameters of the search algorithm*/
+int POP_SIZE;  /* The number of candidate solutions*/
+int MAX_ITER; /*The number of iterations*/
+int FUNCTION;
+//Problem definitions
+int DIM;//number of problem variables
+double *lb;//lower bound of the variables
+double *ub;//upper bound of the variables
+int RUN;/*Algorithm can run many times in order to see its robustness*/
+double bestfoRUN;
+//Global variables
+double **pop;//[POP_SIZE][DIM];  //population of candidate solutions.
+double *fo;//[POP_SIZE];      //objective function value.
+double *best;//[DIM];           //best solution found
+double bestfo;//best fo value
+int best_index;//index for the best solution
+double *y; //constraint functions
+double *c; //penallity function constant
+
+
+//Functions declarations
+double randon(double inferior, double superior);
+void freeArrays();
+int GetParameters(char **argv);
+void showParameters();
+void prepararObjFunc();
+double constr(double sol[]);
+double objfunc(double sol[], int cond);
+void initPop();
+void mutualism_phase(int index_i);
+void commensalism_phase(int index_i);
+void parasitism_phase(int index_i);
+int AvgStdDev(double *Avg,double *StdDev,double Var[]);
+void plot(FILE *shellComands, char *run);
+char *converteDecChar(char *strf, int dec);
+int ffscanf(char *fieldname, FILE *fp, char *format, void *inbuffer);
+
+#endif
