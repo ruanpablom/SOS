ab84e9b72c0809371f882e523c049f15ba663c75
ruanpablom <pm.ruan@gmail.com>
2015-10-02 01:56:46 +0000

    modificado o arquivo time.sh

--- time.sh
+++ time.sh
@@ -3,6 +3,6 @@ for k in `seq 1 8`
 do
     for i in `seq 1 20`;
     do
-        { time ./alg input"$k".in; } 2>> experimento"$k".txt;
+        { time ./alg input"$k".in; } 2>> Experimento/experimento"$k".txt
     done
 done   
